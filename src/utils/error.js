0f7a6c8dd54c05ebaee5588160a06360b037ff12
Sohee Lee <essen030216@naver.com>
2025-10-31 21:50:54 +0900

    feat(utils/error.js): 에러 메시지 상수화

---
+++ src/utils/error.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+  INVALID_AMOUNT: "[ERROR] 구입 금액은 1,000원 단위여야 합니다.",
+  INVALID_RANGE: "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.",
+  INVALID_LENGTH: "[ERROR] 로또 번호는 6개여야 합니다.",
+};
