1d308452045e460a41c088848f8325a225891b56
Sohee Lee <essen030216@naver.com>
2025-10-31 22:42:22 +0900

    feat(domain/LottoGenerator.js): 로또 생성 로직 추가

---
+++ src/domain/LottoGenerator.js
@@ -0,0 +1,25 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "./Lotto.js";
+import { LOTTO } from "../utils/constants.js";
+
+class LottoGenerator {
+  static generateTickets(amount) {
+    const ticketCount = amount / LOTTO.price;
+
+    const tickets = [];
+    for (let i = 0; i < ticketCount; i++) {
+      const numbers = Random.pickUniqueNumbersInRange(
+        LOTTO.MIN,
+        LOTTO.MAX,
+        LOTTO.COUNT
+      );
+
+      const ticket = new Lotto(numbers);
+      tickets.push(ticket);
+    }
+
+    return tickets;
+  }
+}
+
+export default LottoGenerator;
