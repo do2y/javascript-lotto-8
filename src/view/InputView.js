84cb115a413d0f51ee287e3760ef1b0913be4347
Sohee Lee <essen030216@naver.com>
2025-11-01 13:46:45 +0900

    feat(view/InputView.js): 보너스 번호 입력 로직 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -10,6 +10,11 @@ class InputView {
     const input = await Console.readLineAsync("당첨 번호를 입력해 주세요.\n");
     return input.trim();
   }
+
+  static async readBonusNumber() {
+    const input = await Console.readLineAsync("보너스 번호를 입력해 주세요.\n");
+    return input.trim();
+  }
 }
 
 export default InputView;
