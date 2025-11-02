803269670c1118770001c18c7fe5037173c34bce
Sohee Lee <essen030216@naver.com>
2025-11-02 19:18:12 +0900

    feat(domain/Lotto.js): 로또 번호 검증 로직 세분화 및 상수 참조로 개선

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,4 +1,5 @@
-import { ERROR_MESSAGES } from '../utils/error.js';
+import { ERROR_MESSAGES } from "../utils/error.js";
+import { LOTTO } from "../utils/constants.js";
 class Lotto {
   #numbers;
 
@@ -8,8 +9,21 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
+    if (numbers.length !== LOTTO.COUNT) {
       throw new Error(ERROR_MESSAGES.INVALID_LENGTH);
+    }
+
+    if (numbers.some((num) => typeof num !== "number" || Number.isNaN(num))) {
+      throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBER);
+    }
+
+    if (numbers.some((num) => num < LOTTO.MIN || num > LOTTO.MAX)) {
+      throw new Error(ERROR_MESSAGES.INVALID_RANGE);
+    }
+
+    const uniqueNumbers = new Set(numbers);
+    if (uniqueNumbers.size !== numbers.length) {
+      throw new Error(ERROR_MESSAGES.DUPLICATED_NUMBER);
     }
   }
 
