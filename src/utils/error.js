ddbb5a4296824c09e7f2bc099a5a09174cd00c75
Sohee Lee <essen030216@naver.com>
2025-11-02 19:46:25 +0900

    refactor(utils/error.js): 에러 메시지 네이밍 및 문구 명확화

--- src/utils/error.js
+++ src/utils/error.js
@@ -2,6 +2,7 @@ export const ERROR_MESSAGES = {
   INVALID_AMOUNT: '[ERROR] 구입 금액은 1,000원 단위여야 합니다.',
   INVALID_RANGE: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
   INVALID_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
-  DUPLICATED_NUMBER: '[ERROR] 당첨 번호와 중복된 보너스 번호입니다.',
+  DUPLICATED_NUMBER: '[ERROR] 로또 번호가 중복되었습니다.',
+  DUPLICATED_BONUS_NUMBER: '[ERROR] 보너스 번호가 당첨 번호와 중복됩니다.',
   INVALID_WINNING_NUMBER: '[ERROR] 당첨 번호는 1부터 45 사이의 숫자만 가능합니다.',
 };
