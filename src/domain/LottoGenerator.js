a8b8bb3ac616c2d43d0d51e4b25de960c62e0a7a
Sohee Lee <essen030216@naver.com>
2025-11-03 01:50:51 +0900

    fix(domain/LottoGenerator.js): 상수명 수정으로 테스트 통과

--- src/domain/LottoGenerator.js
+++ src/domain/LottoGenerator.js
@@ -4,15 +4,11 @@ import { LOTTO } from '../utils/constants.js';
 
 class LottoGenerator {
   static generateTickets(amount) {
-    const ticketCount = amount / LOTTO.price;
+    const ticketCount = amount / LOTTO.PRICE;
 
     const tickets = [];
     for (let i = 0; i < ticketCount; i++) {
-      const numbers = Random.pickUniqueNumbersInRange(
-        LOTTO.MIN,
-        LOTTO.MAX,
-        LOTTO.COUNT,
-      );
+      const numbers = Random.pickUniqueNumbersInRange(LOTTO.MIN, LOTTO.MAX, LOTTO.COUNT);
 
       const ticket = new Lotto(numbers);
       tickets.push(ticket);
