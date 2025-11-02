42bbf1b0e0784cae324a774b5c6a8d868c88fa3b
Sohee Lee <essen030216@naver.com>
2025-11-03 01:54:21 +0900

    style: 따옴표를 싱글 쿼트로 통일

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -1,15 +1,15 @@
-import Parser from "../src/utils/Parser";
+import Parser from '../src/utils/Parser';
 
-describe("Parser 클래스 테스트", () => {
-  describe("parseLottoNumbers()", () => {
-    test("쉼표(,)로 구분된 숫자 문자열을 배열로 변환한다.", () => {
-      const input = "1,2,3,4,5,6";
+describe('Parser 클래스 테스트', () => {
+  describe('parseLottoNumbers()', () => {
+    test('쉼표(,)로 구분된 숫자 문자열을 배열로 변환한다.', () => {
+      const input = '1,2,3,4,5,6';
       const result = Parser.parseLottoNumbers(input);
       expect(result).toEqual([1, 2, 3, 4, 5, 6]);
     });
 
-    test("공백이 포함된 입력을 정상적으로 처리한다.", () => {
-      const input = " 1,  2 ,3 , 4,5,6 ";
+    test('공백이 포함된 입력을 정상적으로 처리한다.', () => {
+      const input = ' 1,  2 ,3 , 4,5,6 ';
       const result = Parser.parseLottoNumbers(input);
       expect(result).toEqual([1, 2, 3, 4, 5, 6]);
     });
