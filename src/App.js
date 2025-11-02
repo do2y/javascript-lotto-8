a8c59671d98375a59fd31bed6130c6e222cfa8e4
Sohee Lee <essen030216@naver.com>
2025-11-03 03:25:44 +0900

    feat(App.js): 로또 발매기 전체 실행 흐름 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
+import { Console } from '@woowacourse/mission-utils';
+import InputView from './view/InputView.js';
+import Printer from './view/Printer.js';
+import Validator from './utils/Validator.js';
+import LottoGenerator from './domain/LottoGenerator.js';
+import LottoResult from './domain/LottoResult.js';
+import Parser from './utils/Parser.js';
+
 class App {
   async run() {
-    //1. 금액 입력
-    //2. 로또 생성
-    //3. 당첨 번호 입력, 보너스 번호
-    //4. 비교/계산
-    //5. 출력
+    const purchaseAmount = await this.#readPurchaseAmount();
+    const tickets = LottoGenerator.generateTickets(purchaseAmount);
+
+    Printer.printPurchasedAmount(tickets.length);
+    Printer.printLottoTickets(tickets);
+
+    const winningNumbers = await this.#readWinningNumbers();
+    const bonusNumber = await this.#readBonusNumber(winningNumbers);
+
+    const result = new LottoResult(tickets, winningNumbers, bonusNumber);
+    Printer.printStatistics(result.getStatistics());
+    Printer.printProfitRate(result.getProfitRate());
+  }
+
+  async #readPurchaseAmount() {
+    while (true) {
+      try {
+        const input = await InputView.readPurchaseAmount();
+        const amount = Validator.validatePurchaseAmount(input);
+        return amount;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  async #readWinningNumbers() {
+    while (true) {
+      try {
+        const input = await InputView.readLottoNumbers();
+        const parsedNumbers = Parser.parseLottoNumbers(input);
+        Validator.validateWinningNumbers(parsedNumbers);
+        return parsedNumbers;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  async #readBonusNumber(winningNumbers) {
+    while (true) {
+      try {
+        const input = await InputView.readBonusNumber();
+        const parsedNumber = Number(input);
+        Validator.validateBonusNumber(parsedNumber, winningNumbers);
+        return parsedNumber;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   }
 }
 
